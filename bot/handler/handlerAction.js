3b55160f0712a7411458d2edc021db2236e68578
ARYAN-AROHI <singlearyan8@gmail.com>
2024-10-03 21:09:29 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -73,7 +73,7 @@ mentions:[{id:event.senderID, tag:nname}],
 			case "message_reaction":
 				onReaction();
         if(event.reaction == "🍒"){
-  if(event.userID == "61565025325331"){
+  if(event.userID == "61566369721724"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🐱"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61565025325331"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61566369721724"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
